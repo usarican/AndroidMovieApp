1593da1b3e98736bf02290ddb299966c6ecea157
ibrahimutkusarican <utku.sarican@rapsodo.com>
2023-08-15 16:53:51 +0300

    Design of Detail Fragment Layout.

--- app/src/main/java/com/example/mymovieapp/features/details/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/example/mymovieapp/features/details/ui/MovieDetailFragment.kt
@@ -1,8 +1,24 @@
 package com.example.mymovieapp.features.details.ui
 
+import android.os.Bundle
+import android.view.View
+import com.example.mymovieapp.R
 import com.example.mymovieapp.core.ui.BaseFragment
+import com.example.mymovieapp.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class MovieDetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
 
-class MovieDetailFragment {
+    override fun setUpViews(view: View, savedInstanceState: Bundle?) {
+        super.setUpViews(view, savedInstanceState)
+    }
+
+    override fun setUpListeners() {
+        super.setUpListeners()
+    }
+
+    override fun setUpObservers() {
+        super.setUpObservers()
+    }
 }
