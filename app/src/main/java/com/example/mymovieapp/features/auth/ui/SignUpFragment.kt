4874dcff9e0062bdefda2528fd0faaddee70efde
ibrahimutkusarican <utku.sarican@rapsodo.com>
2023-09-14 11:19:44 +0300

    Sign-up page design is finished

---
+++ app/src/main/java/com/example/mymovieapp/features/auth/ui/SignUpFragment.kt
@@ -0,0 +1,9 @@
+package com.example.mymovieapp.features.auth.ui
+
+
+import com.example.mymovieapp.R
+import com.example.mymovieapp.core.ui.BaseFragment
+import com.example.mymovieapp.databinding.FragmentSignUpBinding
+
+class SignUpFragment :  BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
+}
