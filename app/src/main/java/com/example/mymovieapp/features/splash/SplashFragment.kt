c322a94976ddcf2a3fa07ba6b178e7951b1e52a7
usarican <iu.sarican@gmail.com>
2024-02-24 23:21:08 +0300

    Splash navigation added.

---
+++ app/src/main/java/com/example/mymovieapp/features/splash/SplashFragment.kt
@@ -0,0 +1,16 @@
+package com.example.mymovieapp.features.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mymovieapp.R
+import com.example.mymovieapp.core.ui.BaseFragment
+import com.example.mymovieapp.databinding.FragmentSplashBinding
+
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_splash) {
+
+
+}
