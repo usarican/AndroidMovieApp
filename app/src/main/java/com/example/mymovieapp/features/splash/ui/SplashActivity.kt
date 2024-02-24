153c5fb71ce60b0075f48d979e81ebdf61cb648e
usarican <iu.sarican@gmail.com>
2024-02-24 23:30:18 +0300

    Splash navigation added.

--- app/src/main/java/com/example/mymovieapp/features/splash/ui/SplashActivity.kt
+++ app/src/main/java/com/example/mymovieapp/features/splash/ui/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mymovieapp.features.splash
+package com.example.mymovieapp.features.splash.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/example/mymovieapp/features/splash/ui/SplashFragment.kt
+++ app/src/main/java/com/example/mymovieapp/features/splash/ui/SplashFragment.kt
@@ -1,4 +1,4 @@
-package com.example.mymovieapp.features.splash
+package com.example.mymovieapp.features.splash.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
