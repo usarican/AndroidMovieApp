330d7937921cbc5152475d5ec4a99eff11c0bc2c
usarican <iu.sarican@gmail.com>
2024-02-25 23:37:41 +0300

    Making to profile page composable

--- app/src/main/java/com/example/mymovieapp/features/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mymovieapp/features/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import com.example.mymovieapp.R
 import com.example.mymovieapp.core.ui.BaseFragment
@@ -15,6 +16,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
     private lateinit var composeView: ComposeView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val window = activity?.window
+        window?.let { WindowCompat.setDecorFitsSystemWindows(window, false) }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/example/mymovieapp/features/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/mymovieapp/features/profile/ProfileScreen.kt
@@ -1,20 +1,70 @@
 package com.example.mymovieapp.features.profile
 
+import android.content.res.Resources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mymovieapp.R
 
+@Preview(showBackground = true)
 @Composable
-fun ProfileScreen(){
+fun ProfileScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
+        modifier = Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.safeDrawing)
+            .clickable {
+                println("Clicked.")
+            },
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        UserAvatarImage()
         Text(text = "Profile Screen Compose View")
     }
+}
+
+@Composable
+fun UserAvatarImage() {
+    Image(
+        painter = painterResource(id = R.drawable.user_photo),
+        contentDescription = "userAvatar",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape)
+            .border(1.dp, Color.Black, CircleShape)
+    )
 }
