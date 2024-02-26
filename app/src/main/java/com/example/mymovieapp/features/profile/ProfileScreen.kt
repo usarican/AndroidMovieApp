f4447a9c0517fed1e3d192cbebd541d274caf3e7
usarican <iu.sarican@gmail.com>
2024-02-26 11:35:34 +0300

    Learning jetpack compose

--- app/src/main/java/com/example/mymovieapp/features/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/mymovieapp/features/profile/ProfileScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
@@ -20,20 +22,20 @@ import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,6 +44,9 @@ import com.example.mymovieapp.R
 @Preview(showBackground = true)
 @Composable
 fun ProfileScreen() {
+    var userName by remember {
+        mutableStateOf("")
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -53,6 +58,14 @@ fun ProfileScreen() {
     ) {
         UserAvatarImage()
         Text(text = "Profile Screen Compose View")
+        Spacer(modifier = Modifier
+            .height(16.dp)
+            .fillMaxWidth())
+        UserNameField(userName = userName)
+        ChangeUserNameButton(
+            onChanged = {
+            userName = "Utku Sarican"
+        })
     }
 }
 
@@ -67,4 +80,16 @@ fun UserAvatarImage() {
             .clip(CircleShape)
             .border(1.dp, Color.Black, CircleShape)
     )
+}
+
+@Composable
+fun UserNameField(userName: String) {
+    Text(text = userName)
+}
+
+@Composable
+fun ChangeUserNameButton(onChanged: () -> Unit) {
+    Button(onClick = { onChanged.invoke() }) {
+        Text(text = "Change The Name")
+    }
 }
